7c8701c8bcb708f4bc4dc15619cc5272fd8115c5
Sheenam7 <sheenamdubey78@gmail.com>
2024-07-23 11:31:02 +0530

    home commit

--- lib/core/constant/my_assests.dart
+++ lib/core/constant/my_assests.dart
@@ -6,6 +6,7 @@ class MyAssests{
   static const String assetsImageOnboard1 = "assets/images/onboardB (1).jpeg";
   static const String assetsImageOnboard2 = "assets/images/onboardB (2).jpeg";
   static const String assetsImageOnboard3 = "assets/images/onboardB (3).jpeg";
+  static const String assetsImageNetFlix  = "assets/images/NetFlixWeb.jpeg";
 
 
 }
---
+++ lib/presentation/screens/general/home/home.dart
@@ -0,0 +1,28 @@
+import 'package:bogging_app/core/constant/my_assests.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+            children: [Image.asset(MyAssests.assetsImageNetFlix).cornerRadius(20)],
+
+          ),
+        ),
+      ),
+    );
+  }
+}
