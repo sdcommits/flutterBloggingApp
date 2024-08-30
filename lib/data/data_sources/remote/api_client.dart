6dd8d789e7a43397b1271eca72d96e35f2ec86cb
Sheenam7 <sheenamdubey78@gmail.com>
2024-08-31 01:46:06 +0530

    api client commit

--- lib/data/data_sources/remote/api_client.dart
+++ lib/data/data_sources/remote/api_client.dart
@@ -1,24 +1,26 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
+import 'api_constant.dart';
+import 'api_endpoint_urls.dart';
+
 class ApiClient {
 
   getTags() async {
 
     final dio = Dio();
     try {
-      var response = await dio.get('https://api.pub.dev/not-exit');
-      // 404
-      await dio.get('https://api.pub.dev/not-exist');
+      var response = await dio.get(ApiConstant.mainUrl + ApiEndpointUrls.tags);
+      debugPrint(response.statusCode.toString());
+      // debugPrint(response.data);
     } on DioException catch (e) {
       if (e.response != null) {
-        debugPrint(e.response.data);
-        debugPrint(e.response.headers);
-        debugPrint(e.response.requestOptions);
+        debugPrint(e.response!.data);
+        debugPrint(e.response!.headers.toString());
+        debugPrint(e.response!.requestOptions.toString());
       } else {
-        // Something happened in setting up or sending the request that triggered an Error
-        print(e.requestOptions);
-        print(e.message);
+        debugPrint(e.requestOptions.toString());
+        debugPrint(e.message);
       }
     }
 
