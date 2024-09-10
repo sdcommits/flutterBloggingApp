6b164329f164ae197fd1bb3ff50dd3083f41e91f
Sheenam7 <sheenamdubey78@gmail.com>
2024-09-11 01:30:55 +0530

    get api commits

--- lib/data/data_sources/remote/api_client.dart
+++ lib/data/data_sources/remote/api_client.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:bogging_app/data/data_sources/remote/api_exception.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
@@ -7,36 +8,65 @@ import 'api_constant.dart';
 import 'api_endpoint_urls.dart';
 
 class ApiClient {
+  late Dio dio;
+  late BaseOptions baseOptions;
 
-  getTags() async {
+  ApiClient() {
+    final baseOptions = BaseOptions(baseUrl: ApiConstant.mainUrl);
+    final dio = Dio(baseOptions);
+  }
 
 
-
-    final options = BaseOptions(
-      baseUrl : ApiConstant.mainUrl,
-      // dio.options.baseUrl = 'https://api.pub.dev';
-      // dio.options.connectTimeout: Duration(Second: 5);
-    );
+  //Get request
+  Future<Response> getRequest() async {
+    final options = BaseOptions(baseUrl: ApiConstant.mainUrl);
 
     final dio = Dio(options);
 
     try {
       debugPrint("=========API REQUEST============");
-      debugPrint("Request Url: ${options.baseUrl + ApiEndpointUrls.tags}");
+      debugPrint("Request Url: ${baseOptions.baseUrl + ApiEndpointUrls.tags}");
       var response = await dio.get(ApiEndpointUrls.tags);
       debugPrint("=========API RESPONSE============");
       debugPrint("Status Code : ${response.statusCode}");
-      log("DATA: ${response.data}");
+      return response;
     } on DioException catch (e) {
       if (e.response != null) {
         debugPrint(e.response!.data);
         debugPrint(e.response!.headers.toString());
         debugPrint(e.response!.requestOptions.toString());
+        throw ApiException(message: e.response!.statusMessage);
       } else {
         debugPrint(e.requestOptions.toString());
         debugPrint(e.message);
+        throw ApiException(message: e.message);
       }
     }
-
   }
+
+
+  Future<Response> postRequest() async {
+    try {
+      debugPrint("=========API REQUEST============");
+      debugPrint("Request Url: ${baseOptions.baseUrl + ApiEndpointUrls.tags}");
+      var response = await dio.post(ApiEndpointUrls.addTags);
+      debugPrint("=========API RESPONSE============");
+      debugPrint("Status Code : ${response.statusCode}");
+      return response;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        debugPrint(e.response!.data);
+        debugPrint(e.response!.headers.toString());
+        debugPrint(e.response!.requestOptions.toString());
+        throw ApiException(message: e.response!.statusMessage);
+      } else {
+        debugPrint(e.requestOptions.toString());
+        debugPrint(e.message);
+        throw ApiException(message: e.message);
+      }
+    }
+  }
+
 }
+
+
